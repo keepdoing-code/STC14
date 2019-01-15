a146318ccd63eafc1b1b82a087b04c7d31eb41c2
stritron <finalroot@gmail.com>
2019-01-15 20:59:51 +0300

    modified 3

--- src/ru/inno/lec02/ExceptionDemo.java
+++ src/ru/inno/lec02/ExceptionDemo.java
@@ -5,33 +5,6 @@ package ru.inno.lec02;
  */
 public class ExceptionDemo {
     public static void main(String[] args) {
-        A a = new A(new C());
-        a.doSome();
-    }
-}
 
-class A{
-    B b;
-
-    public A(B b) {
-        this.b = b;
-    }
-
-    String doSome(){
-        return b.doSomeElse().toLowerCase();
-    }
-}
-
-class B{
-    String doSomeElse(){
-        return "B do some else";
-    }
-}
-
-class C extends B{
-    @Override
-    String doSomeElse() {
-        return null;
-//        return "C do some else";
     }
 }
