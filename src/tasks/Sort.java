09b0a4c29c78663a8289ee2d6ef6fa9d167df8b8
stritron <finalroot@gmail.com>
2019-01-16 17:25:42 +0300

    Quick sorting

--- src/tasks/Sort.java
+++ src/tasks/Sort.java
@@ -4,16 +4,23 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- * @author stritron
- * Bubble sorting algorithm
  * @param arr - input array to sort
+ * @author stritron
+ *         Bubble sorting algorithm
+ *         Quick sorting algorithm
  */
 public class Sort {
     public static void main(String[] args) {
         Integer arr[] = new Integer[10];
-        fillArray(arr);
-        printArray(arr);
+
+        System.out.println("Bubble sort");
+        fillAndPrintArray(arr);
         bubbleSort(arr);
+        printArray(arr);
+
+        System.out.println("Quick sort");
+        fillAndPrintArray(arr);
+        fastSort(arr, 0, arr.length - 1);
         printArray(arr);
     }
 
@@ -25,7 +32,7 @@ public class Sort {
             sorted = true;
             for (int j = 0; j < arr.length - i - 1; j++) {
 
-                if(arr[j] > arr[j + 1]){
+                if (arr[j] > arr[j + 1]) {
                     Integer a = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = a;
@@ -39,11 +46,40 @@ public class Sort {
     }
 
 
-    public static void fillArray(final Object[] arr) {
+    private static int fastSortHelper(Integer[] arr, int start, int end) {
+        int tmp;
+        int pointer = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] < arr[end]) {
+                tmp = arr[pointer];
+                arr[pointer] = arr[i];
+                arr[i] = tmp;
+                pointer += 1;
+            }
+        }
+        tmp = arr[pointer];
+        arr[pointer] = arr[end];
+        arr[end] = tmp;
+        return pointer;
+    }
+
+    public static void fastSort(Integer[] arr, int start, int end) {
+        if (start > end) {
+            return;
+        }
+
+        int mid = fastSortHelper(arr, start, end);
+        fastSort(arr, start, mid - 1);
+        fastSort(arr, mid + 1, end);
+    }
+
+
+    public static void fillAndPrintArray(final Object[] arr) {
         Random rnd = new Random();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rnd.nextInt(10);
         }
+        printArray(arr);
     }
 
     public static void printArray(final Object[] arr) {
