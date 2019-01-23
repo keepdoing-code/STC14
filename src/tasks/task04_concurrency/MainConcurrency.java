bbbc3c9679c85f97b71ce50627c63669226d565b
stritron <finalroot@gmail.com>
2019-01-23 03:38:00 +0300

    Task 04. Task template.

---
+++ src/tasks/task04_concurrency/MainConcurrency.java
@@ -0,0 +1,13 @@
+package tasks.task04_concurrency;
+
+/**
+ * Created on 23.01.19.
+ *
+ * @author Yuri Lupandin
+ * @version 1.0
+ */
+public class MainConcurrency {
+    public static void main(String[] args) {
+
+    }
+}
