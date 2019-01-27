6b7ac75dba0372d09c18960bffbc0c3fbfafaa47
stritron <finalroot@gmail.com>
2019-01-27 03:30:15 +0300

    Logger added print Map<>

--- src/tasks/task04_concurrency/Loger.java
+++ src/tasks/task04_concurrency/Loger.java
@@ -1,6 +1,8 @@
 package tasks.task04_concurrency;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * Created on 24.01.19.
@@ -9,12 +11,26 @@ import java.time.LocalDateTime;
  * @version 1.0
  */
 public class Loger {
+    private static final String template = "%s : %s \r\n";
+
     public static void i(String str) {
-        String template = "%s : %s \r\n";
         System.out.printf(template, LocalDateTime.now().toString(), str);
     }
 
     public static void i(Integer integer) {
         i(integer.toString());
+    }
+
+    public static void i(Collection list) {
+        for (Object item : list) {
+            i(item.toString());
+        }
+    }
+
+    public static <T> void i(Map<T, T> pairs) {
+        System.out.println(LocalDateTime.now());
+        for (Map.Entry e : pairs.entrySet()) {
+            System.out.printf(template, e.getKey().toString(), e.getValue().toString());
+        }
     }
 }
